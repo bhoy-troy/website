c6ce1d707dc33ddb66d4c390dc8eb58c899f97b9
James Troy <james.troy@jci.com>
2021-02-22 20:20:48 +0000

    add google analytics

--- layouts/default.vue
+++ layouts/default.vue
@@ -37,6 +37,17 @@
 
     }
   }
+
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+
+  gtag('js', new Date());
+
+  gtag('config', 'G-EWYCNYZE05');
+
 </script>
 
 <style>
