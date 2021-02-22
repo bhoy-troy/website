6fe43449b852a749f21bc70585f10b7008564afe
bhoy-troy <5635593+bhoy-troy@users.noreply.github.com>
2021-02-22 10:08:14 +0000

    replace default text

--- pages/index.vue
+++ pages/index.vue
@@ -6,24 +6,9 @@
         nuxtjs
       </h1>
       <h2 class="subtitle">
-        My astonishing Nuxt.js project
+        Nuxt.js project in progress
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <div class="links"></div>
     </div>
   </div>
 </template>
